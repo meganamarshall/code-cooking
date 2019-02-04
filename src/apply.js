2a33394cfd60ade8003936ecc6cb67bda891a90c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-04 11:32:32 -0800

    added peanut allergy section

--- src/apply.js
+++ src/apply.js
@@ -1,19 +1,31 @@
 const formNode = document.getElementById('form-example');
 const nameNode = document.getElementById('name');
 const zipCodeNode = document.getElementById('zip-code');
-// another way to do this
-// const nameValue = inputNode.value;
+const peanutRangeNode = document.getElementById('peanuts');
+const allergicNumberNode = document.getElementById('allergic-number');
+const yesNode = document.getElementById('yes');
+const noNode = document.getElementById('no');
 
-// another way to do this
-// const nameValue = formNode.elements.name.value;
+peanutRangeNode.addEventListener('change', function() {
+    allergicNumberNode.textContent = peanutRangeNode.value;
+});
+
+yesNode.addEventListener('change', function() {
+    
+});
+
+noNode.addEventListener('change', function() {
+    
+});
 
 formNode.addEventListener('submit', function(event) {
     event.preventDefault();
 
     const applicant = {
+        //name=key, whole thing is "key value pair"
         name: nameNode.value,
-        zipCode: zipCodeNode.value
+        zipCode: zipCodeNode.value,
+        allergicScale: peanutRangeNode.value
     };
 
-    console.log(applicant);
 })
